23731ede8618893b297ae5f6dc04a394cf2a36ef
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-26 08:56:38 +0000

    Prefer '{}' to '[list]' when creating empty lists

--- www/admin/index.tcl
+++ www/admin/index.tcl
@@ -10,7 +10,7 @@ ad_page_contract {
 }
 
 set page_title "[_ glossar.Glossar_Administration]"
-set context [list]
+set context {}
 
 set categories_node_id [db_string get_category_node_id {}]
 set categories_url [site_node::get_url -node_id $categories_node_id]
